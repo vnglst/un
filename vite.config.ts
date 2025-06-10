60369cacc4d9dcdd1228f9f8e2ae15e5db3185c5
Koen van Gilst <koen@koenvangilst.nl>
2025-06-10 15:37:05 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [reactRouter(), tailwindcss()],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./app"),
+      '~': path.resolve(__dirname, './app'),
     },
   },
   server: {
     port: 3000,
   },
-});
+})
